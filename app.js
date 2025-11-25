97084db69de215e7c895df53c0bcc2f926b26499
ather ali <a.ngaming102734@gmail.com>
2025-11-25 16:44:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// alert('ather ali')
+
+// var Name ='ather ali'
+
+// document.write (Name)
+
+// var myName
+// var my-Name
+// var my/Name
+// var my$Name
+// var 1myName
+// var my_Name 
+
+//  var firstName = "new "
+// var lastName = "world"
+
+// document.write (firstName+ ' ' + lastName)
+// document.write (firstName+  lastName)
+
+// console.log (firstName)
+
+// var student1 = 12;
+
+// var student2 = 15;
+
+// var newTotal = student1 * student2
+
+// document.write (newTotal)
+
+// var num1 = 3 % 1;
+
+// document .write (num1)
+
+// var num1 =10;
+
+// num1 ++;
+
+// document.write (num1+200)
+
+
+//  var message
+//  = "Thanks for your input!";
+//   alert(message
+//  );
+
+// alert("Thanks, " + userName 
+    // + "!");
+
+    var spec = prompt
+ ("Your species?", "human");
+
+ console.log()
